d4cc5114961c7cb2480a9ef3ceefce5463e212c3
Nata <38166392+NataKrass@users.noreply.github.com>
2021-06-21 21:45:12 +0300

    ajax

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./index.css";
 
-import App from './components/app/app.js';
-
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
